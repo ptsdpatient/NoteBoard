cbe998541a9dfcdf06312e5358f18e8ce4a66ecc
ptsdpatient <orbit.technologies.nagpur@gmail.com>
2024-08-12 11:21:37 +0530

    layout changes

--- src/components/Whiteboard.js
+++ src/components/Whiteboard.js
@@ -137,64 +137,94 @@ const Whiteboard = () => {
     return (
         <div className=" p-4 lg:p-8 bg-gradient-to-r from-blue-50 to-purple-50 min-h-screen">
             <h1 className="text-5xl font-bold mb-8 text-gray-800 text-center">Whiteboard</h1>
-            <div className="mb-8 flex justify-center space-x-4">
-                <button
-                    onClick={() => handleToolChange('brush')}
-                    className={`px-6 py-3 rounded-lg shadow-md transition-transform ${tool === 'brush' ? 'bg-gradient-to-r from-blue-600 to-indigo-600 text-white' : 'bg-white text-gray-800 border border-gray-300'} hover:scale-105`}
-                    aria-label="Brush Tool"
-                >
-                    Brush
-                </button>
-                <button
-                    onClick={() => handleToolChange('eraser')}
-                    className={`px-6 py-3 rounded-lg shadow-md transition-transform ${tool === 'eraser' ? 'bg-gradient-to-r from-blue-600 to-indigo-600 text-white' : 'bg-white text-gray-800 border border-gray-300'} hover:scale-105`}
-                    aria-label="Eraser Tool"
-                >
-                    Eraser
-                </button>
+           
+            <div className="mb-8 flex flex-col md:flex-row justify-between">
+                <div class="flex flex-wrap justify-around gap-3">
+                    <button
+                        onClick={() => handleColorChange('#000000')}
+                        className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#000000' ? 'bg-black text-white' : 'bg-gray-200'} `}
+                        aria-label="Black Brush Color"
+                        disabled={tool === 'eraser'}
+                    >
+                        Black
+                    </button>
+                    <button
+                        onClick={() => handleColorChange('#FF0000')}
+                        className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#FF0000' ? 'bg-red-600 text-white' : 'bg-red-200'} `}
+                        aria-label="Red Brush Color"
+                        disabled={tool === 'eraser'}
+                    >
+                        Red
+                    </button>
+                    <button
+                        onClick={() => handleColorChange('#00FF00')}
+                        className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#00FF00' ? 'bg-green-600 text-white' : 'bg-green-200'} `}
+                        aria-label="Green Brush Color"
+                        disabled={tool === 'eraser'}
+                    >
+                        Green
+                    </button>
+                    <button
+                        onClick={() => handleColorChange('#0000FF')}
+                        className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#0000FF' ? 'bg-blue-600 text-white' : 'bg-blue-200'} `}
+                        aria-label="Blue Brush Color"
+                        disabled={tool === 'eraser'}
+                    >
+                        Blue
+                    </button>
+                    <button
+                        onClick={() => handleColorChange('#FFFF00')}
+                        className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#FFFF00' ? 'bg-yellow-600 text-black' : 'bg-yellow-200'} `}
+                        aria-label="Yellow Brush Color"
+                        disabled={tool === 'eraser'}
+                    >
+                        Yellow
+                    </button>
+                </div>
+
+                <div class="flex flex-row justify-around gap-4">
+                    <button  onClick={() => handleToolChange('brush')}
+                            className={`px-6 py-3 rounded-lg shadow-md transition-transform ${tool === 'brush' ? 'bg-gradient-to-r from-blue-600 to-indigo-600 text-white' : 'bg-white text-gray-800 border border-gray-300'} hover:scale-105`}
+                            aria-label="Brush Tool"
+                                >
+                                    Brush
+                    </button>
+                    <button
+                                    onClick={() => handleToolChange('eraser')}
+                                    className={`px-6 py-3 rounded-lg shadow-md transition-transform ${tool === 'eraser' ? 'bg-gradient-to-r from-blue-600 to-indigo-600 text-white' : 'bg-white text-gray-800 border border-gray-300'} hover:scale-105`}
+                                    aria-label="Eraser Tool"
+                                >
+                                    Eraser
+                    </button>
+                </div>
+
+                <div class="flex flex-wrap gap-3 justify-around">
+                    <button
+                        onClick={handleSave}
+                        disabled={isSaving}
+                        className={`px-6 py-2 rounded-lg transition ${isSaving ? 'bg-gray-500' : 'bg-blue-600 hover:bg-blue-700'} text-white`}
+                        aria-busy={isSaving}
+                    >
+                        {isSaving ? 'Saving...' : (editingImage ? 'Save Changes' : 'Save')}
+                    </button>
+                    <button
+                        onClick={handleDownload}
+                        className="px-6 py-3 rounded-lg bg-green-600 text-white shadow-md transition-transform hover:scale-105"
+                        aria-label="Download Drawing"
+                    >
+                        Download
+                    </button>
+                    <button
+                        onClick={handleClear}
+                        className="px-6 py-3 rounded-lg bg-red-600 text-white shadow-md transition-transform hover:scale-105"
+                        aria-label="Clear Drawing"
+                    >
+                        Clear
+                    </button>
+                </div>
             </div>
-            <div className="mb-8 flex justify-center flex-wrap space-x-4">
-                <button
-                    onClick={() => handleColorChange('#000000')}
-                    className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#000000' ? 'bg-black text-white' : 'bg-gray-200'} `}
-                    aria-label="Black Brush Color"
-                    disabled={tool === 'eraser'}
-                >
-                    Black
-                </button>
-                <button
-                    onClick={() => handleColorChange('#FF0000')}
-                    className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#FF0000' ? 'bg-red-600 text-white' : 'bg-red-200'} `}
-                    aria-label="Red Brush Color"
-                    disabled={tool === 'eraser'}
-                >
-                    Red
-                </button>
-                <button
-                    onClick={() => handleColorChange('#00FF00')}
-                    className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#00FF00' ? 'bg-green-600 text-white' : 'bg-green-200'} `}
-                    aria-label="Green Brush Color"
-                    disabled={tool === 'eraser'}
-                >
-                    Green
-                </button>
-                <button
-                    onClick={() => handleColorChange('#0000FF')}
-                    className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#0000FF' ? 'bg-blue-600 text-white' : 'bg-blue-200'} `}
-                    aria-label="Blue Brush Color"
-                    disabled={tool === 'eraser'}
-                >
-                    Blue
-                </button>
-                <button
-                    onClick={() => handleColorChange('#FFFF00')}
-                    className={`px-4 my-2 py-2 rounded-full ${tool === 'eraser' ? 'bg-gray-300' : brushColor === '#FFFF00' ? 'bg-yellow-600 text-black' : 'bg-yellow-200'} `}
-                    aria-label="Yellow Brush Color"
-                    disabled={tool === 'eraser'}
-                >
-                    Yellow
-                </button>
-            </div>
+
+
             <div className="bg-white shadow-lg rounded-lg mb-8 p-4 mx-auto" style={{ width: '100%', height: '600px' }}>
                 <CanvasDraw
                     ref={handleCanvasRef}
@@ -206,30 +236,7 @@ const Whiteboard = () => {
                     style={{ width: '100%', height: '100%' }}
                 />
             </div>
-            <div className="mb-8 flex justify-center space-x-4">
-                <button
-                    onClick={handleSave}
-                    disabled={isSaving}
-                    className={`px-6 py-2 rounded-lg transition ${isSaving ? 'bg-gray-500' : 'bg-blue-600 hover:bg-blue-700'} text-white`}
-                    aria-busy={isSaving}
-                >
-                    {isSaving ? 'Saving...' : (editingImage ? 'Save Changes' : 'Save')}
-                </button>
-                <button
-                    onClick={handleDownload}
-                    className="px-6 py-3 rounded-lg bg-green-600 text-white shadow-md transition-transform hover:scale-105"
-                    aria-label="Download Drawing"
-                >
-                    Download
-                </button>
-                <button
-                    onClick={handleClear}
-                    className="px-6 py-3 rounded-lg bg-red-600 text-white shadow-md transition-transform hover:scale-105"
-                    aria-label="Clear Drawing"
-                >
-                    Clear
-                </button>
-            </div>
+            
             {error && <p className="text-red-600 text-center">{error}</p>}
             {images.length > 0 ? (
                 <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
